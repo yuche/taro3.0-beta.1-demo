fbf0fdf9fedf676e0a698fd2c2461c5a226e2c23
yiqing.chen <yiqing.chen@weimob.com>
2020-03-30 16:56:29 +0800

    demo

---
+++ src/app.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Provider } from 'mobx-react'
+import { Current } from '@tarojs/taro'
+
+import counterStore from './store/counter'
+
+import './app.scss'
+
+const store = {
+  counterStore
+}
+
+class App extends Component {
+  componentDidMount () {
+    console.log(util2)
+  }
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // this.props.children 就是要渲染的页面
+  render () {
+    return (
+      <Provider store={store}>
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+
+export default App
